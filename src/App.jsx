127124f80edbb5e2e2d97293753bf89db7a6628a
Mustafa Ali <mustafaaaly27@gmail.com>
2024-09-25 10:09:05 +0500

    tanstack query installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+import axios from 'axios'
 import './App.css'
+import { useQuery } from '@tanstack/react-query'
 
 function App() {
 
+  // now if we wanna fetch the api we gotta make a use effect for getting the api then making a state to store it
+  // then we gotta make another state for loader then another for error handling then conditions etc 
+  // react query can handle all that by itself 
+  // process? first install there libray by going to tanstack query and install it then go to main.jsx file and make entire web query client provide children
+
+
+// Now example how it works 
+const fetchingApi = ()=>{
+const res = axios.get("https://fakestoreapi.com/products")
+return res
+
+}
+
+const{data : products ,isLoading ,isError}= useQuery({ // now data: products means im destructuring the data key from object and changing the key name from data to products
+  queryKey : ["Products"],
+  queryFn : fetchingApi,
+})
+console.log(products);
+
   return (
     <>
-    
 
 
-    
+
+
     </>
   )
 }
